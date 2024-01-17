eeef2d64701bfa92c8d4b6c176311b1ae3bf7761
Yaquier Mansfarroll <=>
2024-01-17 17:12:42 +0100

    feat: Execute initial migration in prisma

--- src/lib/entities/product/model/Product.models.ts
+++ src/lib/entities/product/model/Product.models.ts
@@ -2,6 +2,10 @@ import type { Cost } from '$lib/entities/product/types';
 
 export class Product implements Cost {
 	constructor(
+		// Own properties.
+		public readonly id: number,
+		public provider: string,
+		// Cost interface-like properties.
 		public category: string,
 		public amount: number,
 		public date: Date | string,
